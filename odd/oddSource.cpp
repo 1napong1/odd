a4cced5075c0a1e1ce901b8931d3ae4ef8e0a5e5
Napong Keha <gorngorn9876@hotmail.com>
2020-09-11 19:14:19 +0700

    Add project files.

---
+++ odd/oddSource.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	double num;
+	scanf_s("%d", &num);
+	if (num % 2 == 0)
+	{
+		printf("even");
+	}
+	else if (num % 2 == 1)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("Error");
+	}
+
+}
